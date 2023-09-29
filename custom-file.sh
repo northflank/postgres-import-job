69867f05d9917dd05bfea839a0404e340fcb6d19
Cameron McDermott <cameron_mcdermott@yahoo.co.uk>
2023-09-29 13:58:18 +0100

    Update custom-file.sh

--- custom-file.sh
+++ custom-file.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env ash
 
 set -e
-echo "===== $(date)"
-pg_dump -F${DUMP_FORMAT:-c} ${DUMP_FLAGS} "${SRC_POSTGRES}" > ${BASE_PATH}/dump
+if [ ! -f ${BASE_PATH}/dump ] || [ "${FORCE_DUMP}" == "true" ]; then
+  echo "===== $(date)"
+  pg_dump -F${DUMP_FORMAT:-c} ${DUMP_FLAGS} "${SRC_POSTGRES}" > ${BASE_PATH}/dump
+fi
 echo "===== $(date)"
 pg_restore ${RESTORE_FLAGS} "${DST_POSTGRES}" ${BASE_PATH}/dump
 echo "===== $(date)"
