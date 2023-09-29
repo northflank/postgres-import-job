c6970d3cb3185a3762d0a9c905d5676938a751bc
Cameron McDermott <cameron_mcdermott@yahoo.co.uk>
2023-09-29 11:49:03 +0100

    Update custom-streamed.sh

--- custom-streamed.sh
+++ custom-streamed.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-pg_dump -Fc ${DUMP_FLAGS} "${SRC_POSTGRES}" | pg_restore ${RESTORE_FLAGS} "${DST_POSTGRES}"
+pg_dump -F${DUMP_FORMAT:-c} ${DUMP_FLAGS} "${SRC_POSTGRES}" | pg_restore ${RESTORE_FLAGS} "${DST_POSTGRES}"
