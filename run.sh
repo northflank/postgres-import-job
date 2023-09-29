36a085cec28f5b53414e97957e4b2a2a154e7083
Cameron McDermott <cameron_mcdermott@yahoo.co.uk>
2023-09-29 11:30:55 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,16 +2,4 @@
 
 set -e
 
-PGPASS_FOLDER=`dirname ${PGPASSFILE}`
-
-mkdir -p ${PGPASS_FOLDER}
-
-echo "${HOST}:${PORT}:${DATABASE}:${USER}:${PASSWORD}" > $PGPASSFILE
-
-chmod 0600 $PGPASSFILE
-
-ls -al ${PGPASS_FOLDER}
-
-cat $PGPASSFILE
-
-pg_dumpall ${DUMP_FLAGS} | psql "${DST_POSTGRES}"
+pg_dump ${DUMP_FLAGS} "${SRC_POSTGRES}" | psql "${DST_POSTGRES}"
