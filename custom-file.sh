9dde8cd476d440738e03d9c3aca2531b8abbafa1
Cameron McDermott <cameron_mcdermott@yahoo.co.uk>
2023-09-29 11:48:56 +0100

    Update custom-file.sh

--- custom-file.sh
+++ custom-file.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-pg_dump -Fc ${DUMP_FLAGS} "${SRC_POSTGRES}" > ${BASE_PATH}/dump
+pg_dump -F${DUMP_FORMAT:-c} ${DUMP_FLAGS} "${SRC_POSTGRES}" > ${BASE_PATH}/dump
 pg_restore ${RESTORE_FLAGS} "${DST_POSTGRES}" ${BASE_PATH}/dump
