2349ad0b001b4e8acf551da0208d4b2374fae78b
Cameron McDermott <cameron_mcdermott@yahoo.co.uk>
2023-10-03 22:24:06 +0100

    Update custom-file.sh

--- custom-file.sh
+++ custom-file.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env ash
+set +e
 
-set -e
 if [ ! -f ${BASE_PATH}/dump ] || [ "${FORCE_DUMP}" == "true" ]; then
   echo "===== $(date)"
   pg_dump -F${DUMP_FORMAT:-c} ${DUMP_FLAGS} "${SRC_POSTGRES}" > ${BASE_PATH}/dump
@@ -8,3 +8,9 @@ fi
 echo "===== $(date)"
 pg_restore ${RESTORE_FLAGS} -d "${DST_POSTGRES}" ${BASE_PATH}/dump
 echo "===== $(date)"
+
+echo "DONE"
+if [ "$SLEEP" != "" ]; then
+  echo "sleeping"
+  sleep 500000
+fi
