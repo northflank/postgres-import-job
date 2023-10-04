585f05dc6ccc8d1ec06ca6f3b83fefeff0548784
Cameron McDermott <cameron_mcdermott@yahoo.co.uk>
2023-10-04 17:36:38 +0100

    Update custom-file-filter-matview.sh

--- custom-file-filter-matview.sh
+++ custom-file-filter-matview.sh
@@ -6,10 +6,10 @@ if [ ! -f ${BASE_PATH}/dump ] || [ "${FORCE_DUMP}" == "true" ]; then
   pg_dump -F${DUMP_FORMAT:-c} ${DUMP_FLAGS} "${SRC_POSTGRES}" > ${BASE_PATH}/dump
 fi
 echo "===== $(date)"
-pg_restore -l -Fd ${BASE_PATH}/dump | sed '/MATERIALIZED VIEW DATA/d' > ordered.lst
+pg_restore -l -Fc ${BASE_PATH}/dump | sed '/MATERIALIZED VIEW DATA/d' > ordered.lst
 pg_restore -L ordered.lst ${RESTORE_FLAGS} -d "${DST_POSTGRES}" ${BASE_PATH}/dump
 if [ "${DO_REFRESH}" == "true" ]; then
-  pg_restore -l -Fd ${BASE_PATH}/dump | grep 'MATERIALIZED VIEW DATA' > refresh.lst
+  pg_restore -l -Fc ${BASE_PATH}/dump | grep 'MATERIALIZED VIEW DATA' > refresh.lst
   pg_restore -L refresh.lst ${RESTORE_FLAGS} -d "${DST_POSTGRES}" ${BASE_PATH}/dump
 fi
 echo "===== $(date)"
