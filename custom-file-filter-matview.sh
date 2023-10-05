662cdd3a3a31f62ea2a5c4a8167455cd612d9cbd
Cameron McDermott <cameron_mcdermott@yahoo.co.uk>
2023-10-05 09:27:30 +0100

    Update custom-file-filter-matview.sh

--- custom-file-filter-matview.sh
+++ custom-file-filter-matview.sh
@@ -7,13 +7,13 @@ if [ ! -f ${BASE_PATH}/dump ] || [ "${FORCE_DUMP}" == "true" ]; then
 fi
 echo "===== $(date)"
 if [ "${SKIP_MATVIEW}" == "true" ]; then
-  pg_restore -l -Fc ${BASE_PATH}/dump | sed '/MATERIALIZED VIEW/d' > ordered.lst
+  pg_restore -l -F${DUMP_FORMAT:-c} ${BASE_PATH}/dump | sed '/MATERIALIZED VIEW/d' > ordered.lst
 else
-  pg_restore -l -Fc ${BASE_PATH}/dump | sed '/MATERIALIZED VIEW DATA/d' > ordered.lst
+  pg_restore -l -F${DUMP_FORMAT:-c} ${BASE_PATH}/dump | sed '/MATERIALIZED VIEW DATA/d' > ordered.lst
 fi
 pg_restore -L ordered.lst ${RESTORE_FLAGS} -d "${DST_POSTGRES}" ${BASE_PATH}/dump
 if [ "${DO_REFRESH}" == "true" ] && [ "${SKIP_MATVIEW}" != "true" ]; then
-  pg_restore -l -Fc ${BASE_PATH}/dump | grep 'MATERIALIZED VIEW DATA' > refresh.lst
+  pg_restore -l -F${DUMP_FORMAT:-c} ${BASE_PATH}/dump | grep 'MATERIALIZED VIEW DATA' > refresh.lst
   pg_restore -L refresh.lst ${RESTORE_FLAGS} -d "${DST_POSTGRES}" ${BASE_PATH}/dump
 fi
 echo "===== $(date)"
